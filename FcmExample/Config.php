4d5762bcce714e6bad913a956ff1f2b8f413517c
fahmi <fahmiahmadf97@gmail.com>
2019-10-12 17:19:21 +0700

    update config fcm

--- FcmExample/Config.php
+++ FcmExample/Config.php
@@ -1,10 +1,15 @@
 <?php
-define('DB_USERNAME','root');
+/*define('DB_USERNAME','root');
 define('DB_PASSWORD','');
 define('DB_NAME','simadesa');
 define('DB_HOST','localhost');
+*/
+define('DB_USERNAME','desababa_simadesa');
+define('DB_PASSWORD','.]J[Dj2BZ8&M');
+define('DB_NAME','desababa_simadesa');
+define('DB_HOST','localhost');
 
-
+//$connn = mysqli_connect("localhost","desababa_simadesa",".]J[Dj2BZ8&M","desababa_simadesa");
 define('FIREBASE_API_KEY','AAAADwL7df8:APA91bGR4rltqMvp68b9tuAyxGreaFLDd1D8xiMa3o3Uqhm3oGgchiyQpDdyo-fEzi49puGB4G3hkSg-jV2RI31r5eNAJNnaCTFtvIL-UtafAiU61e0nWbVIw30dkooS8TMt3DmsCKs3');
 
 
